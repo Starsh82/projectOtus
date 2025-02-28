5586f717855e154fd91c3b3f3b8d6640780520bb
Starsh <starshmail@yandex.ru>
2025-02-28 11:14:49 +0000

    script p1

--- scripts/serv_p1.sh
+++ scripts/serv_p1.sh
@@ -2,4 +2,13 @@
 
 #Установка nginx
 apt install -y nginx
-systemctl enable --now nginx.service
+systemctl enable nginx.service
+
+#Копирование рабочих конфигов и перезпуск nginx
+cp -r /root/projectOtus/configs/serv_p1/nginx/* /etc/nginx/
+systemctl restart nginx.service
+
+#Установка prometheus-node-exporter
+echo "!!!!!Установка prometheus-node-exporter!!!!!"
+apt install -y prometheus-node-exporter
+systemctl enable --now prometheus-node-exporter
