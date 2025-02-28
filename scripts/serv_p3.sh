e2d7c92dcecd9bd62301d9099b065c4fe9612218
Starsh <starshmail@yandex.ru>
2025-02-28 12:37:25 +0000

    serv_p2 p3

--- scripts/serv_p3.sh
+++ scripts/serv_p3.sh
@@ -2,9 +2,9 @@
 
 #Установка apache2
 apt install apache2
-cp /root/projectOtus/configs/serv_p2/apache2/*.conf /etc/apache2/
-cp -r /root/projectOtus/configs/serv_p2/apache2/sites-* /etc/apache2/
-cp -r /root/projectOtus/configs/serv_p2/apache2/html1 /var/www/
+cp /root/projectOtus/configs/serv_p3/apache2/*.conf /etc/apache2/
+cp -r /root/projectOtus/configs/serv_p3/apache2/sites-* /etc/apache2/
+cp -r /root/projectOtus/configs/serv_p3/apache2/html1 /var/www/
 systemctl enable --now apache2.service
 
 #Установка prometheus-node-exporter
