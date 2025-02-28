1646a27b141ea59427c4e90b8c0b02d0fc2aaf8a
Starsh <starshmail@yandex.ru>
2025-02-28 12:32:00 +0000

    serv_p2

--- scripts/serv_p2.sh
+++ scripts/serv_p2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-apt install apache2
 
+#Установка apache2
+apt install apache2
+cp /root/projectOtus/configs/serv_p2/apache2/*.conf /etc/apache2/
+cp -r /root/projectOtus/configs/serv_p2/apache2/sites-* /etc/apache2/
+cp -r /root/projectOtus/configs/serv_p2/apache2/html1 /var/www/
 systemctl enable --now apache2.service
