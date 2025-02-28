9726deedd21774159077669f72eea889f8c12a1c
Starsh82 <starshmail@yandex.ru>
2025-02-28 22:33:41 +0400

    Update serv_p4.sh

--- scripts/serv_p4.sh
+++ scripts/serv_p4.sh
@@ -16,6 +16,7 @@ systemctl start grafana-server
 #Установка ES
 dpkg -i /opt/elasticsearch_8.9.1_amd64-224190-f79e75.deb
 #Память+конфиги
+cp -r ~/projectOtus/configs/serv_p4/elasticsearch/* /etc/elasticsearch/
 systemctl daemon-reload
 systemctl enable --now elasticsearch.service
 
@@ -23,21 +24,11 @@ systemctl enable --now elasticsearch.service
 dpkg -i /opt/kibana_8.9.1_amd64-224190-f7ebba.deb
 systemctl daemon-reload
 systemctl enable --now kibana.service
-#sudo nano /etc/kibana/kibana.yml
-#server.port: 5601
-#server.host: "0.0.0.0"
+cp ~/projectOtus/configs/serv_p4/kibana/kibana.yml /etc/kibana/
 systemctl restart kibana
 
 #Установка logstash
 dpkg -i /opt/logstash_8.9.1_amd64-224190-11b1b0.deb
-ystemctl enable --now logstash.service
-#sudo nano /etc/logstash/logstash.yml
+sytemctl enable --now logstash.service
+cp -r ~/projectOtus/configs/serv_p4/logstash/* /etc/logstash/
 systemctl restart logstash.service
-
-
-#Установка prometheus-node-exporter
-#echo "!!!!!Установка prometheus-node-exporter!!!!!"
-#apt install -y prometheus-node-exporter
-#systemctl enable --now prometheus-node-exporter
-
-#Установка grafana
