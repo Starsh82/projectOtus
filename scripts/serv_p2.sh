8a009714bbe854b4adabe167eadca0c40c881d3d
Starsh82 <starshmail@yandex.ru>
2025-03-01 09:41:04 +0400

    Update serv_p2.sh

--- scripts/serv_p2.sh
+++ scripts/serv_p2.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 #Установка apache2
+echo "Установка apache2"
 apt install -y apache2
 cp /root/projectOtus/configs/serv_p2/apache2/*.conf /etc/apache2/
 cp -r /root/projectOtus/configs/serv_p2/apache2/sites-* /etc/apache2/
 cp -r /root/projectOtus/configs/serv_p2/apache2/html1 /var/www/
 systemctl enable --now apache2.service
+echo "apache2 установлен"
 
 #Установка prometheus-node-exporter
-echo "!!!!!Установка prometheus-node-exporter!!!!!"
+echo "Установка prometheus-node-exporter"
 apt install -y prometheus-node-exporter
 systemctl enable --now prometheus-node-exporter
+echo "prometheus-node-exporter установлен"
 
 #Установка и настройка MySQL
 "Установка mysql-server-8.0 (slave)"
