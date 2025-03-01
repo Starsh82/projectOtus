3f5bb67a82d75ec5fd8aa5d0edd12be498f95ea2
Starsh82 <starshmail@yandex.ru>
2025-03-01 11:24:15 +0400

    Update and rename backup_mysql to backup_mysql.sh

--- scripts/backup_mysql.sh
+++ scripts/backup_mysql.sh
@@ -3,17 +3,18 @@
 #set -Eeuo pipefail
 
 #for varDB in `mysql -N -e "show databases like '%\_db';"`
-for varDB in `mysql -N -e "show databases;"`
-do
+#for varDB in `mysql -N -e "show databases;"`
+#do
 #	echo $varDB
 # mysqldump не может сделать дамп DB information_schema: mysqldump: Dumping 'information_schema' DB content is not supported
-	if [ "$varDB" !=  "information_schema" ]
-	then
-		mkdir $varDB
+#	if [ "$varDB" !=  "information_schema" ]
+#	then
+		varDB="employees"
+  		mkdir $varDB
 		for varT in `mysql -N -e "show tables from $varDB;"`
 		do
 #			echo $varT
 			mysqldump --opt --single-transaction --events --routines $varDB $varT > $varDB/$varT.sql
 		done
-	fi
-done
+	#fi
+#done
