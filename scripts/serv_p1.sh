106e5e13783b812d1930fa63d89e1ffae0bab0db
Starsh82 <starshmail@yandex.ru>
2025-03-01 10:09:30 +0400

    Update serv_p1.sh

--- scripts/serv_p1.sh
+++ scripts/serv_p1.sh
@@ -31,6 +31,6 @@ systemctl start mysql
 mysql_secure_installation
 cp ~/projectOtus/configs/serv_p1/MySQL/mysqld.cnf /etc/mysql/mysql.conf.d/
 systemctl restart mysql
-mysql -e 'CREATE USER 'repl'@'%' IDENTIFIED WITH 'caching_sha2_password' BY '123qwe';'
-mysql -e 'GRANT REPLICATION SLAVE ON *.* TO repl@'%';'
+mysql -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'Pass1234#1234';"
+mysql -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';"
 echo "mysql-server-8.0 установлен"
